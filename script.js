2fd43996359ca8d3f45afc947e88ff4b9ef3adc8
hajarlaanab4 <laanabhajar@gmail.com>
2025-07-26 18:49:58 +0100

    Premier commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer","Backend developer","full-stack ","problem solving"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+    
+})
